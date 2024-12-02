9578bacf830ee5f0f9777dac0400cba895c8db9a
GizmoGear22 <mpsimmer@gmail.com>
2024-12-01 20:45:48 -0500

    Attempting to migrate EntityFramework

---
+++ DataLayer/DataAccessHandlers/DataAccessHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer.DatabaseAccess;
+using Microsoft.Extensions.Configuration;
+
+namespace DataLayer.DataAccessHandlers
+{
+    public class DataAccessHandler
+    {
+        private readonly DBAccess _dbAccess;
+        private readonly IConfiguration _config;
+        public DataAccessHandler(DBAccess dbAccess, IConfiguration config)
+        {
+            _dbAccess = dbAccess;
+            _config = config;
+        }
+    }
+}
--- DataLayer/DatabaseAccess/DBAccess.cs
+++ DataLayer/DatabaseAccess/DBAccess.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Models;
 
 namespace DataLayer.DatabaseAccess
 {
-    public class DBAccess
+    public class DBAccess : DbContext
     {
+        public DBAccess(DbContextOptions<DBAccess> options) : base(options)
+        { }
+        public DbSet<LoginModel> LoginCredentials { get; set; }
+
     }
 }
