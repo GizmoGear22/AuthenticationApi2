9dfa8dcdda3b1917c766030546b871ccf421b313
GizmoGear22 <mpsimmer@gmail.com>
2024-12-03 17:49:22 -0500

    Added Api Auth Middleware for key

--- AuthenticationApi/Program.cs
+++ AuthenticationApi/Program.cs
@@ -1,11 +1,44 @@
+using AuthenticationLayer;
+using DataLayer.DatabaseAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//DbContext
+builder.Services.AddDbContext<DBAccess>(options => { options.UseSqlServer(builder.Configuration.GetConnectionString("DbCnnString")); });
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = "X-Api-Key",
+        Type = SecuritySchemeType.ApiKey,
+        Description = "Input Api Key Here"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "ApiKey"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+}
+
+);
 
 var app = builder.Build();
 
@@ -17,6 +50,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseMiddleware<ApiAuthMiddleware>();
 
 app.UseAuthorization();
 
---
+++ AuthenticationLayer/ApiAuthMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace AuthenticationLayer
+{
+    public class ApiAuthMiddleware
+    {
+        public readonly RequestDelegate _next;
+        private readonly IConfiguration _config;
+        private const string apiHeaderKey = "X-Api-Key";
+
+        public ApiAuthMiddleware(RequestDelegate next, IConfiguration config)
+        {
+            _next = next;
+            _config = config;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+
+            var apiKey = _config.GetValue<string>("ApiKey");
+            if (!context.Request.Headers.TryGetValue(apiHeaderKey, out var value))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Input Api Key");
+                return;
+            }
+            if (!apiKey.Equals(value))
+            {
+                context.Response.StatusCode = 402;
+                await context.Response.WriteAsync("Wrong Api Key");
+                return;
+            }
+
+            await _next(context);
+        }
+            
+    }
+}
